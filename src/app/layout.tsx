58b4991be852a65eab574d86c9332cecaf94ac92
Andrea Pilone <9wggprpq@protonmail.com>
2025-09-17 17:10:02 +0200

    fix: aggiunto head mancante in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body style={{ margin:0, background:'#111' }}>{children}</body>
+      <head />
+      <body style={{ margin:0, background:"#111" }}>{children}</body>
     </html>
   );
 }
+
